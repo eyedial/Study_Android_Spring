7b694051b1308d06acd6592ae6135cac8c3e4c01
chosoonseob <eyedial@naver.com>
2021-05-14 14:49:32 +0900

    모두 완성

--- SpringWorkspace/AnSpring_App/src/main/java/com/csslect/app/controller/AController.java
+++ SpringWorkspace/AnSpring_App/src/main/java/com/csslect/app/controller/AController.java
@@ -1,20 +1,10 @@
 package com.csslect.app.controller;
 
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
-import javax.imageio.ImageIO;
-import javax.imageio.stream.ImageOutputStream;
 import javax.servlet.http.HttpServletRequest;
-import org.jcodec.api.FrameGrab;
-import org.jcodec.api.JCodecException;
-import org.jcodec.common.model.Picture;
-import org.jcodec.scale.AWTUtil;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
